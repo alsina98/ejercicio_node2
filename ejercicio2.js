adb32549185a63e8fbcb99c2ba429359852cf378
Jordi <91033826+alsina98@users.noreply.github.com>
2022-12-06 12:11:38 +0100

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,23 @@
+let nombre = "Jordi"; 
+console.log(nombre);
+
+let edat = 24;
+console.log(edat);
+
+let isdeveloper = true;
+console.log(isdeveloper);
+
+const d  = new Date("march 06 1998 17:34");
+console.log(d);
+
+const libro = {
+    titulo: "Los pilares de la tierra",
+    autor: "Ken Follett",
+    fecha: new Date("1989"),
+    url: new URL("https://www.amazon.es/pilares-Tierra-Saga-BEST-SELLER/dp/8466341781/ref=asc_df_8466341781/?tag=googshopes-21&linkCode=df0&hvadid=195345636420&hvpos=&hvnetw=g&hvrand=17432895324837730261&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1005471&hvtargid=pla-316487079040&psc=1")
+}
+
+console.log(libro.titulo);
+console.log(libro.autor);
+console.log(libro.fecha);
+console.log(libro.url);
